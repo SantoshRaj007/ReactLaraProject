11ca7497ffc68bce1752b7864c78dad511cf7823
unknown <admin@JMJGN-96.dcjmjain.com>
2024-12-25 18:32:02 +0530

    frontend file

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './components/Home'
+import Shop from './components/Shop'
+
+function App() {
+    return (
+        <>
+           <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/shop' element={<Shop />} />
+                </Routes>
+           </BrowserRouter>
+        </>
+    )
+}
+
+export default App
